4015cc27ce1415ad60a5fb88dd870da82891b8da
angeljs95 <angelsuarez2612@gmail.com>
2023-11-08 19:39:28 -0300

    Probando escribir en el main! :)

--- src/main/java/com/egg/servicios/ServiciosApplication.java
+++ src/main/java/com/egg/servicios/ServiciosApplication.java
@@ -9,5 +9,9 @@ public class ServiciosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiciosApplication.class, args);
 	}
-        
+
+	//hola mundo
+	//que estan hacien??
+	//do??v
+	//se ve algo??
 }
