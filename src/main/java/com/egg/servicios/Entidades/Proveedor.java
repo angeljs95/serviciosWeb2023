25af4312f4f4b976d20e3525805742ac71c64b12
Mariela <melodyesteche@gmail.com>
2023-11-14 18:37:22 -0300

    Cambios nuevos en el Form

---
+++ src/main/java/com/egg/servicios/Entidades/Proveedor.java
@@ -0,0 +1,93 @@
+
+package com.egg.servicios.Entidades;
+
+import java.util.ArrayList;
+
+public class Proveedor extends Usuario {
+    
+    String profesion;
+    Double costoHora;
+    String matricula;
+    Integer cbu;
+    Integer puntuacion;
+    ArrayList<String> comentarios;
+    ArrayList<Cliente> clientes;
+    String descripcion;
+
+
+    public Proveedor() {
+        super();
+    }
+
+    
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public Double getCostoHora() {
+        return costoHora;
+    }
+
+    public void setCostoHora(Double costoHora) {
+        this.costoHora = costoHora;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Integer getCbu() {
+        return cbu;
+    }
+
+    public void setCbu(Integer cbu) {
+        this.cbu = cbu;
+    }
+
+    public Integer getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(Integer puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public ArrayList<String> getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(ArrayList<String> comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(ArrayList<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+            
+            
+    
+    
+}
