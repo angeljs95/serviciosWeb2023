3a68e5e6f0e23a64cd2292cf0d31ce056b9aee97
RodrigoCena <rodrigocena1999@gmail.com>
2023-11-27 11:54:03 -0300

    guardando cambios

--- src/main/java/com/egg/servicios/SeguridadWeb.java
+++ src/main/java/com/egg/servicios/SeguridadWeb.java
@@ -41,7 +41,6 @@ public class SeguridadWeb extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/admin/*").hasRole("ADMIN")
                 .antMatchers("/css/*", "/js/*", "/img/*", "/**")
                 .permitAll()
                 .and().formLogin()
